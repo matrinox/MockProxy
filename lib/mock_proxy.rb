afd4dac03dd39cd35bd99ce293a940cfd55295b7
matrinox <geoff.lee@lendesk.com>
2016-08-15 08:55:07 -0700

    Change valid_callback_tree? to instance method ----------------------------------------------------------- On branch master - Mon 15 Aug 2016 08:55:07 PDT by matrinox <geoff.lee@lendesk.com>

--- lib/mock_proxy.rb
+++ lib/mock_proxy.rb
@@ -249,18 +249,17 @@ class MockProxy
   #
   # @param [#to_s => AnyObject, !Hash] callback_tree
   # @return [Boolean] true if callback is valid, false if not
-  def self.valid_callback_tree?(callback_tree)
+  def valid_callback_tree?(callback_tree)
     return false unless callback_tree.is_a?(Hash)
     callback_tree.all? do |key, value|
       next false unless key.responds_to?(:to_s)
       value.is_a?(Hash) ? valid_callback_tree?(value) : valid_callback?(value)
     end
   end
-  private_class_method :valid_callback_tree?
 
   # @param [Hash] callback_hash the tree of chained method calls
   def initialize(callback_hash)
-    self.class.valid_callback_tree?(callback_hash)
+    valid_callback_tree?(callback_hash)
     @callback_hash = callback_hash.deep_stringify_keys.freeze
   end
 
